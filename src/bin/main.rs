e283e7bdd5b3053e091bca0ca810ec277f399dd4
tmoneypenny <4460992+tmoneypenny@users.noreply.github.com>
2022-05-08 09:57:57 -0700

    refactor

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,49 +1,50 @@
 use std::error::Error;
-
-use clap::{Parser, Subcommand};
-
-#[derive(Parser)]
-#[clap(author = "Todd Moneypenny", version = "0.1.0")]
-#[clap(about="Document what works", long_about = None)]
-#[clap(propagate_version = true)]
-struct Cli {
-    /// Group commands together
-    #[clap(short, long)]
-    group: bool,
-    #[clap(subcommand)]
-    command: Commands,
-}
-
-#[derive(Subcommand)]
-enum Commands {
-    /// Document last N markers
-    #[clap(parse(try_from_str))]
-    Last { lines: Option<usize> },
-    /// Set a marker to save the last N commands
-    S { save: Option<usize> },
-}
+use std::fs;
+extern crate skim;
+use skim::prelude::*;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::io::SeekFrom;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let cli = Cli::parse();
+    let options = SkimOptionsBuilder::default()
+        .preview_window(Some("up:10%"))
+        .preview(Some(""))
+        .header(Some("SELECT:    enter     | EXIT:         esc\n\
+                      PAGE UP:   page up   | PAGE DOWN:    page down \n\
+                      SELECT UP: shift+tab | SELECT DOWN:  tab\n\
+                      ──────────────────────────────────────────────────────────────────────────────",
+        ))
+        .multi(true)
+        .build()
+        .unwrap();
 
-    let group = match cli.group {
-        true => true,
-        false => false,
-    };
-
-    let cmd_name = std::env::args().next().unwrap();
     let hist_file =
         std::env::var_os("HISTFILE").unwrap_or_else(|| std::ffi::OsString::from(".zsh_history"));
     #[allow(deprecated)]
     let home_dir = std::env::home_dir().expect("could not determine a home directory");
     let path = home_dir.join(&hist_file);
+    let file_len = fs::metadata(&path)?.len();
+    let mut f = File::open(path)?;
 
-    match &cli.command {
-        Commands::Last { lines } => match lines {
-            Some(l) => rmd::run(cmd_name, *l, path, group)?,
-            None => rmd::run(cmd_name, 5, path, group)?,
-        },
-        Commands::S { save: _ } => (),
+    if file_len > 4096 {
+        f.seek(SeekFrom::End(-4096))?;
     }
+
+    let mut bf_path = BufReader::new(f);
+
+    let mut temp_buffer = String::new();
+    bf_path.read_line(&mut temp_buffer).expect("EOF");
+
+    let item_reader = SkimItemReader::default();
+    let items = item_reader.of_bufread(bf_path);
+
+    let selected_items = Skim::run_with(&options, Some(items))
+        .map(|out| out.selected_items)
+        .unwrap_or_else(|| Vec::new());
+
+    rmd::doc(selected_items, true)?;
+
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,16 @@
 use easy_reader::EasyReader;
 use regex::Regex;
-use std::io::Error;
+use skim::prelude::*;
+use skim::SkimItem;
+use std::error::Error;
 
 struct MdReader {
-    lines_read: usize,
     line_buf: Vec<String>,
 }
 
 impl MdReader {
     fn new() -> MdReader {
         MdReader {
-            lines_read: 0,
             line_buf: Vec::<String>::new(),
         }
     }
@@ -31,114 +31,21 @@ impl MdReader {
     }
 }
 
-pub fn run(
-    cmd_name: String,
-    lines_to_read: usize,
-    file: std::path::PathBuf,
-    group: bool,
-) -> Result<(), Error> {
-    let re_capture = Regex::new(r"(?P<name>[:]\s\d{0,10}[:]\d[;])(?P<cmd>.*)").unwrap();
-
-    let re_save = Regex::new(&format!(
-        r"^:\s\d{{0,10}}[:]\d[;]({})\s[s]{{1}}\s{{0,}}\d{{0,}}$",
-        cmd_name
-    ))
-    .unwrap();
-
-    let re_save_group = Regex::new(&format!(
-        r"(?P<zsh>^[:]\s\d{{0,10}}[:]\d[;])(?P<cmd_name>{})(?P<save>\s[s]{{1}}\s{{0,}})(?P<lines>\d{{0,}})",
-        cmd_name
-    ))
-    .unwrap();
-
+pub fn doc(history: Vec<Arc<dyn SkimItem>>, group: bool) -> Result<(), Box<dyn Error>> {
     let mut md_reader = MdReader::new();
+    let zsh_capture_cmd = Regex::new(r"(?P<name>[:]\s\d{0,10}[:]\d[;])(?P<cmd>.*)").unwrap();
 
-    let file = std::fs::File::open(file)?;
-    let mut reader = EasyReader::new(file)?;
+    history.into_iter().for_each(|x| {
+        let cmd = &x.output();
+        let line = zsh_capture_cmd.captures(cmd).unwrap();
+        md_reader
+            .line_buf
+            .push(line.name("cmd").unwrap().as_str().to_string());
+    });
 
-    reader.eof();
-    while let Some(line) = reader.prev_line()? {
-        if re_save.is_match(&line) {
-            if let Some(nl) = re_save_group.captures(&line).unwrap().name("lines") {
-                for _ in 0..nl.as_str().parse::<usize>().unwrap_or(1) {
-                    let line: &String = &reader.prev_line()?.unwrap();
-                    let line = re_capture.captures(&line).unwrap();
-                    md_reader
-                        .line_buf
-                        .insert(0, line.name("cmd").unwrap().as_str().to_owned());
-                }
-            }
-            md_reader.lines_read += 1;
-        }
-
-        if md_reader.lines_read == lines_to_read {
-            break;
-        }
-    }
-
-    if group {
-        md_reader.generate_group_md();
-    } else {
-        md_reader.generate_ind_md();
-    }
-
+    match group {
+        false => md_reader.generate_ind_md(),
+        true => md_reader.generate_group_md(),
+    };
     Ok(())
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    use assert_fs::prelude::*;
-    #[test]
-    fn test_run_multiline() {
-        let cmd_name = String::from("rmd");
-        let temp_file = assert_fs::NamedTempFile::new("histfile").unwrap();
-        temp_file
-            .write_str(
-                ": 1645910774:0;echo \"save this_1\"\n\
-: 1645910780:0;rmd s\n\
-: 1645910785:0;echo \"ignore_this\"\n\
-: 1645910786:0;echo \"ignore_this\"\n\
-: 1645910790:0;echo \"save_this_2\"\n\
-: 1645910810:0;echo \"save_this_2\"\n\
-: 1645910813:0;rmd s 2\n\
-: 1645910815:0;rmd -g last 2",
-            )
-            .unwrap();
-        assert_eq!((), run(cmd_name, 3, temp_file.to_path_buf(), true).unwrap());
-        /*
-        ```sh
-        echo "save this_1"
-        echo "save_this_2"
-        echo "save_this_2"
-        ```
-        */
-        temp_file.close().unwrap();
-    }
-
-    #[test]
-    fn test_run_single_line() {
-        let cmd_name = String::from("rmd");
-        let temp_file = assert_fs::NamedTempFile::new("histfile").unwrap();
-        temp_file
-            .write_str(
-                ": 1645910774:0;echo \"save this\"\n\
-: 1645910780:0;rmd s\n\
-: 1645910785:0;echo \"ignore\"\n\
-: 1645910786:0;echo \"ignore\"\n\
-: 1645910790:0;echo \"ignore\"\n\
-: 1645910810:0;echo \"save this\"\n\
-: 1645910813:0;rmd s\n\
-: 1645910815:0;rmd -g last 2",
-            )
-            .unwrap();
-        assert_eq!((), run(cmd_name, 3, temp_file.to_path_buf(), true).unwrap());
-        /*
-        ```sh
-        echo "save this"
-        echo "save this"
-        */
-        temp_file.close().unwrap();
-    }
 }
